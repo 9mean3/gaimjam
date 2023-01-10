6703b7852627e7d012c6e455c34c0cce5701eb0e
dksgudwn <dksgudwn620@gmail.com>
2023-01-10 19:08:09 +0900

    Player Animation

---
+++ Assets/01_Scripts/Ahn/UIManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] string _sceneName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void sceneLoad()
+    {
+        SceneManager.LoadScene(_sceneName);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}

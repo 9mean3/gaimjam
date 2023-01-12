c983754e9c65631289d47d388eb1b59e65f98240
dksgudwn <dksgudwn620@gmail.com>
2023-01-12 18:30:54 +0900

    111222333

--- Assets/01_Scripts/Ahn/SoundManager.cs
+++ Assets/01_Scripts/Ahn/SoundManager.cs
@@ -1,22 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SoundManager : MonoBehaviour
 {
+    public static SoundManager instance;
     public AudioSource musicsource;
 
     public AudioSource btnsource;
+    public AudioMixer masterMixer;
+    public Slider slider;
 
-    public void SetMusicVolume(float volume)
+    private void Awake()
     {
-        musicsource.volume = volume;
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+            Destroy(this);
     }
 
-    public void SetButtonVolume(float volume)
+    public void ControlVolume()
     {
-        musicsource.volume =volume;
+        float sound = slider.value;
+        if (sound == -40f)
+        {
+            masterMixer.SetFloat("Master", -80f);
+        }
+        else
+        {
+            masterMixer.SetFloat("Master", 0f);
+        }
     }
+
 
     public void OnSfx()
     {
